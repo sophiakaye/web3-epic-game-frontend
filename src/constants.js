daffda7f0b835a919f9886a7cf56b2f01cab4007
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-08-30 00:14:42 +0300

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x1ad5a144b09355B35EFD165a5DbCF9aCc13C876E';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformCharacterData };
